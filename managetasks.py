feef28e4d89d0e37021385a76f3b90411dc8806d
Ken Swanson <kenbswanson@gmail.com>
2025-10-16 01:17:58 -0500

    Basic proof of concept to retrieve API data

--- managetasks.py
+++ managetasks.py
@@ -1,3 +1,14 @@
+import os
+
+api_token=os.environ["TODOIST_API_TOKEN"]
+
 from todoist_api_python.api import TodoistAPI
 
-api = TodoistAPI()
+api = TodoistAPI(api_token)
+print(api)
+
+projects = api.get_projects()
+for project in projects:
+    print("PROJECT")
+    for proj in project:
+        print("   " + proj.name)
