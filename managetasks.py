a1e46633e17b9187e0fe7f8ea23760e89c31b5e7
Ken Swanson <kenbswanson@gmail.com>
2025-10-16 13:33:04 -0500

    Fleshed out code to find the daily tasks to evaluate

--- managetasks.py
+++ managetasks.py
@@ -7,8 +7,20 @@ from todoist_api_python.api import TodoistAPI
 api = TodoistAPI(api_token)
 print(api)
 
-projects = api.get_projects()
-for project in projects:
-    print("PROJECT")
-    for proj in project:
-        print("   " + proj.name)
+def find_tasks_i_do_every_day():
+    query_filter="#Do\ Every\ Day & /Tasks\ To\ Do\ Every\ Day"
+    tasks=[t for tl in api.filter_tasks(query=query_filter) for t in tl ]
+    return tasks
+
+
+def find_everyday_tasks_to_create(tasks):
+    exit(1)
+
+# Find tasks to do every day
+tasks_i_do_every_day=find_tasks_i_do_every_day()
+# print(tasks_i_do_every_day)
+for x in tasks_i_do_every_day:
+    print(x.content)
+
+# Filter out tasks already in "Today" project
+tasks_to_add=find_everyday_tasks_to_create(tasks_i_do_every_day)
