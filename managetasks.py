4cc808dc144570f7a51b4a35b95eb1653e891f79
Ken Swanson <kenbswanson@gmail.com>
2025-10-17 01:34:47 -0500

    Managed the every day tasks

--- managetasks.py
+++ managetasks.py
@@ -8,10 +8,11 @@ if loglevel:
     sh=logging.StreamHandler()
     logger.addHandler(sh)
 
-
+every_day_label="Every Day"
 api_token=os.environ["TODOIST_API_TOKEN"]
 
 from todoist_api_python.api import TodoistAPI
+from todoist_api_python import models
 
 api = TodoistAPI(api_token)
 
@@ -26,10 +27,14 @@ def find_project_by_name(name: str):
     project = find_one_expected(api.get_projects(), lambda p: p.name == name)
     return project
 
+def find_section_in_project(project: models.Project, section_name: str):
+    section = find_one_expected(api.get_sections(project_id=project.id), lambda s: s.name == section_name)
+    return section
+
 do_every_day_project=find_project_by_name("Do Every Day")
 logger.debug(f"do_every_day_project = {do_every_day_project}")
 
-tasks_to_do_every_day_section=find_one_expected(api.get_sections(project_id=do_every_day_project.id), lambda s: s.name == "Tasks To Do Every Day")
+tasks_to_do_every_day_section=find_section_in_project(do_every_day_project, "Tasks To Do Every Day")
 logger.debug(f"tasks_to_do_every_day_section = {tasks_to_do_every_day_section}")
 
 tasks_i_do_every_day=[task for tasks in api.get_tasks(section_id=tasks_to_do_every_day_section.id) for task in tasks]
@@ -43,7 +48,7 @@ for x in tasks_i_do_every_day:
 today_project=find_project_by_name("Today")
 logger.debug(f"today_project = {today_project}")
 
-every_day_tasks_in_today={task.content: task for tasks in api.get_tasks(project_id=today_project.id, label="Every Day") for task in tasks}
+every_day_tasks_in_today={task.content: task for tasks in api.get_tasks(project_id=today_project.id, label=every_day_label) for task in tasks}
 logger.debug(f"every_day_tasks_in_today = {every_day_tasks_in_today}")
 
 # Then, find all tasks without a matching mate in Today
@@ -55,5 +60,11 @@ for task in tasks_i_do_every_day:
         logger.debug(f"task {task.id} ({task.content:<40}) was not found in Today, marking it for addition")
         tasks_to_add.append(task)
 
+print(f"Found {len(tasks_to_add)} tasks to add")
 
-print([task.content for task in tasks_to_add])
+# Add the tasks
+incoming_section=find_section_in_project(today_project, "Incoming")
+logger.debug(f"incoming_section = {incoming_section}")
+for task in tasks_to_add:
+    print(f"\tAdding task '{task.content}'")
+    api.add_task(project_id=today_project.id, section_id=incoming_section.id, content=task.content, labels=[every_day_label])
